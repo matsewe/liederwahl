5dd4b48529f47a6962a490f3495fa19547d357c5
matthias@matsewe.de <matthias@matsewe.de>
2024-06-12 15:01:58 +0200

    Adapt for youtube shorts

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -33,7 +33,7 @@ def get_youtube_id(url):
     youtube_regex = (
         r'(https?://)?(www\.)?'
         '(youtube|youtu|youtube-nocookie)\.(com|be)/'
-        '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+        '(watch\?v=|embed/|shorts/|v/|.+\?v=)?([^&=%\?]{11})')
 
     youtube_regex_match = re.match(youtube_regex, url)
     if youtube_regex_match:
